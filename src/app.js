8cf9f1de1bc21dd6d075144e9b072945c60a1d78
AndreGTHB <andregthb@gmail.com>
2021-04-08 13:35:07 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+
+
+
+ReactDOM.render(<p>This is boilerplatasdye</p>, document.getElementById("app"));
+
+
+
+
+
+
+// class OldSyntax {
+//     constructor(props) {
+//         this.name = 'mike';
+//             }
+//     }
+
+//     const oldSyntax = new OldSyntax();
+//     console.log(oldSyntax);
+
+
+
+//     class NewSyntax {
+//         name = 'jen';
+//     }
+
+//     const newSyntax = new NewSyntax();
+//     console.log(newSyntax);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+console.log(path.join(__dirname, 'public'));
+
+module.exports = {
+    entry: './src/app.js',
+    mode: "development",
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js' // nazywamy jak chcemy ale ta jest dobra
+    },
+    
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/,
+        },{
+            test: /\.s?css$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+    {
+        // mode: 'development',
+    }]
+        
+    },
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+    }
+    
+};
+
