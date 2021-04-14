2e2d5a06c51f4c64fcc33f6a5589187040ef6ece
AndreGTHB <andregthb@gmail.com>
2021-04-14 13:45:14 +0200

    testing css animation, test square box in about page, scss files not splited yet to separated functionality files

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h1>This is About Page</h1>
-        </div>
-    )
-}
+  return (
+    <div class="container">
+      <div class="box">
+        <h1>This is About Page</h1>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
